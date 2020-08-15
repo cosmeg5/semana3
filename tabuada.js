6edb66a7ecf764213ad640cdc5b768b004d339a4
Cosme Barbosa <cosmeg5@hotmail.com>
2020-08-15 07:33:41 -0300

    Fazendo a tabuada ficar limpa e preencher com novos dados

--- tabuada.js
+++ tabuada.js
@@ -3,25 +3,27 @@ function tabuada() {
     let numero = parseInt(document.getElementById("num").value);
     let operador = document.getElementById("operador").value;
     console.log(typeof(numero));
+    let mostrarTabuada = document.getElementById("textTabuada");
+    mostrarTabuada.innerHTML = "";
 
     for(x = 0; x <= 10; x++){
-        
+
         switch(operador){
             case "adicao":
                 let soma = [numero + x];
-                document.getElementById("textTabuada").innerHTML += `<table><tr>${numero} + ${x} = ${soma}</tr></table><br>`;
+                mostrarTabuada.innerHTML += `<table><tr>${numero} + ${x} = ${soma}</tr></table><br>`;
                 break;
             case "-":
                 let subitracao = [numero - x];
-                document.getElementById("textTabuada").innerHTML += `<table><tr>${numero} - ${x} = ${subitracao}</tr></table><br>`;
+                mostrarTabuada.innerHTML += `<table><tr>${numero} - ${x} = ${subitracao}</tr></table><br>`;
                 break;
             case "*":
                 let multiplicacao = [numero * x];
-                document.getElementById("textTabuada").innerHTML += `<table><tr>${numero} x ${x} = ${multiplicacao}</tr></table><br>`;
+                mostrarTabuada.innerHTML += `<table><tr>${numero} x ${x} = ${multiplicacao}</tr></table><br>`;
                 break;
             case "/":
                 let divisao = [numero / x];
-                document.getElementById("textTabuada") += `<table><tr>${numero} ÷ ${x} = ${divisao}</tr></table><br>`;
+                mostrarTabuada.innerHTML += `<table><tr>${numero} ÷ ${x} = ${divisao}</tr></table><br>`;
                 break;
         }
     }
